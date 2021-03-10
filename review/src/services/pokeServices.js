9f581aaab51edef63ad36f0424ba4187a9862646
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-10 12:38:02 -0500

    lecture work-in-progress

--- review/src/services/pokeServices.js
+++ review/src/services/pokeServices.js
@@ -1,8 +1,11 @@
-const getPokemen = ()=>{
+import axios from 'axios';
+import data from "../../data";
+
+export const getPokemen = ()=>{
     return data;
 }
 
-const getPokemon = (id)=>{
+export const getPokemon = (id)=>{
     return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then((res) => {
             return(res.data);
