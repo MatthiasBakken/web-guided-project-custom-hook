8bae7f4f16c48f9bd44ffd5e909bc04434297fb1
kirkby <kirkby@neighbor.com>
2021-03-15 21:00:31 -0600

    progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -29,7 +29,7 @@ export default function SignupForm() {
   // const [email, setEmail] = useState('');
   // const [] = useForm(); ??
   // const [phone, setPhone] = useState('');
-  const [firstName, setFirstName, email.setEmail] = useForm();
+  const [firstName, setFirstName, email, setEmail] = useForm();
 
 
   const handleChanges = e => {
