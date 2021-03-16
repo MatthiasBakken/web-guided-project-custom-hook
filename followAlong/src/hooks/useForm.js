90925e1dcfeb3709bedb8641c3e9c007a245fec7
kirkby <kirkby@neighbor.com>
2021-03-15 20:58:46 -0600

    progress

---
+++ followAlong/src/hooks/useForm.js
@@ -0,0 +1,6 @@
+export const useForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState('');
+
+  return [firstName, setFirstName, email, setEmail];
+}
