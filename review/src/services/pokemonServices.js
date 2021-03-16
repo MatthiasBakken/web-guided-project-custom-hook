8a6fd532bdfe67fbe00cdaf98ebd1d734e647489
kirkby <kirkby@neighbor.com>
2021-03-15 20:03:11 -0600

    progress

--- review/src/services/pokemonServices.js
+++ review/src/services/pokemonServices.js
@@ -1,10 +1,17 @@
 import data from "./../../data";
 
 const fetchSelectedPoke = (id) => {
-  return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((res) => {
-      return(res);
+  // return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+  //   .then((res) => {
+  //     return(res);
+  // });
+  const selectedPoke = data.find(pokemon => {
+    if (id === pokemon.id) {
+      return true;
+    }
+    return false;
   });
+  return Promise.resolve(selectedPoke);
 };
 
 const fetchAllPoke = () => {
