6e1f7431cf143795ee60f6e823a39bbd22b5166b
kirkby <kirkby@neighbor.com>
2021-03-15 21:28:58 -0600

    progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-export const useLocalStorage = (initialValue) => {
+export const useLocalStorage = (key, initialValue) => {
   const [value, setStoredValue] = useState(initialValue);
 
   const setValue = value => {
     setStoredValue(value);
-    window.localStorage.setItem('local-storage-item', JSON.stringify(value));
+    window.localStorage.setItem(key, JSON.stringify(value));
   }
 
   return [value, setValue];
