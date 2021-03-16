7289e9993675d4cc52fcceb3f9e35444b56b3783
kirkby <kirkby@neighbor.com>
2021-03-15 21:27:35 -0600

    progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (initialValue) => {
-  const [value, setValue] = useState(initialValue);
+  const [value, setStoredValue] = useState(initialValue);
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem('loval-storage-item', JSON.stringify(value));
+  }
 
   return [value, setValue];
 }
