55e6eaa7d0101f1bb96755b6f19670c189218ebf
kirkby <kirkby@neighbor.com>
2021-05-10 20:33:39 -0600

    progress!!

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
 import Button from "../theme/Button";
+import { useForm } from "../hooks/useForm";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -25,6 +26,7 @@ export default function SignupForm() {
   const classes = useStyles();
   // const [firstName, setFirstName] = useState("");
   // const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName, lastName, setLastName] = useForm();
 
   const handleChanges = e => {
     setFirstName(e.target.value);
