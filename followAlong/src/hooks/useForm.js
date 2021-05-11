501572c4a86f07988b14f2626b4a69aace65cd90
kirkby <kirkby@neighbor.com>
2021-05-10 20:49:31 -0600

    progress!!

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -3,5 +3,12 @@ import { useState } from 'react';
 export const useForm = () => {
   const [state, setState] = useState({});
 
+  const handleChanges = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return [state, setState, handleChanges, clearForm];
 }
